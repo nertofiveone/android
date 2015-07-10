e7299dab7981093cd8f952ec7f23ea141f8b913d
masensio <masensio@solidgear.es>
2015-07-10 08:36:26 +0200

    Sync account from shortcuts widget

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -99,7 +99,6 @@ import com.owncloud.android.utils.DisplayUtils;
 import com.owncloud.android.utils.ErrorMessageAdapter;
 import com.owncloud.android.utils.MimeTypeUtil;
 import com.owncloud.android.utils.PermissionUtil;
-import com.owncloud.android.widgets.ShortcutsWidget;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -168,6 +167,7 @@ public class FileDisplayActivity extends HookActivity
     private SearchView searchView;
     private boolean mUploadFromWidget = false;
     private boolean mNewFromWidget = false;
+    private boolean mRefreshFromWidget = false;
 
     
     @Override
